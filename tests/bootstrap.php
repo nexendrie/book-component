d46c0a07004df1ed0fc6d092432af03f25466fc3
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-07-17 16:09:07 +0200

    set up for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . "/../vendor/autoload.php";
+Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator) {
+  $configurator->createRobotLoader()->addDirectory([__DIR__ . "/../src"])
+    ->register();
+  $configurator->addParameters(["appDir" => __DIR__,]);
+});
+?>
