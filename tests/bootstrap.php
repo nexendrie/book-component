321ef23155f452fd03df35839abb804760f1aeed
Jakub Konečný <jakub.konecny2@centrum.cz>
2016-09-22 21:43:54 +0200

    do not use RobotLoader in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,6 @@
 require __DIR__ . "/../vendor/autoload.php";
 Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator) {
   $configurator->addParameters(["appDir" => __DIR__,]);
+  $configurator->addConfig(__DIR__ . "/tests.neon");
 });
 ?>
