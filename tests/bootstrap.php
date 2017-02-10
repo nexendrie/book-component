21bded36f8518573cd282a7fd1780bb5504482cd
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-02-10 08:40:12 +0100

    use strict types

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 require __DIR__ . "/../vendor/autoload.php";
 Testbench\Bootstrap::setup(__DIR__ . '/_temp', function (\Nette\Configurator $configurator) {
   $configurator->addParameters(["appDir" => __DIR__,]);
