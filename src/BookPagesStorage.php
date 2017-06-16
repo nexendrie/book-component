6ebd96ed10b23abcead0992520ac6f6818f8f52d
Jakub Konečný <jakub.konecny2@centrum.cz>
2017-06-16 10:13:21 +0200

    used Collection from nexendrie/utils as base for BookPagesStorage, this closes #6

--- src/BookPagesStorage.php
+++ src/BookPagesStorage.php
@@ -3,24 +3,14 @@ declare(strict_types=1);
 
 namespace Nexendrie\BookComponent;
 
+use Nexendrie\Utils\Collection;
+
 /**
  * @author Jakub Konečný
  */
-class BookPagesStorage extends \Nette\Utils\ArrayList {
-  /**
-   * @param mixed $index
-   * @param BookPage $page
-   * @throws \InvalidArgumentException
-   * @throws \RuntimeException
-   */
-  function offsetSet($index, $page) {
-    if(!$page instanceof BookPage) {
-      throw new \InvalidArgumentException("Argument must be of type HelpPage.");
-    } elseif($this->hasPage($page->slug)) {
-      throw new \RuntimeException("Duplicate slug $page->slug.");
-    }
-    parent::offsetSet($index, $page);
-  }
+class BookPagesStorage extends Collection {
+  protected $class = BookPage::class;
+  protected $uniqueProperty = "slug";
   
   /**
    * @param string $slug
