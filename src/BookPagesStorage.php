576305a555eef8e9be717aef44b8496a52b45833
Jakub Konečný <jakub.konecny2@centrum.cz>
2018-12-28 16:03:46 +0100

    simplified methods BookPagesStorage::hasPage() and getAllowedItems()

--- src/BookPagesStorage.php
+++ src/BookPagesStorage.php
@@ -15,12 +15,7 @@ class BookPagesStorage extends Collection {
   protected $uniqueProperty = "slug";
   
   public function hasPage(string $slug): bool {
-    foreach($this->getAllowedItems() as $page) {
-      if($page->slug === $slug) {
-        return true;
-      }
-    }
-    return false;
+    return $this->hasItems(["slug" => $slug]);
   }
   
   public function getIndex(string $slug): ?int {
@@ -36,9 +31,7 @@ class BookPagesStorage extends Collection {
    * @return BookPage[]
    */
   public function getAllowedItems(): array {
-    return array_values(array_filter($this->items, function(BookPage $item) {
-      return $item->isAllowed();
-    }));
+    return $this->getItems(["allowed" => true]);
   }
 }
 ?>
