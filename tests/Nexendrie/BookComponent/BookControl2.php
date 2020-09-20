4336bf6e8ab5a3e571f66271e44f9c3feffa3575
Jakub Konečný <jakub.konecny2@centrum.cz>
2020-09-20 12:36:30 +0200

    added some return type hints

--- tests/Nexendrie/BookComponent/BookControl2.php
+++ tests/Nexendrie/BookComponent/BookControl2.php
@@ -6,7 +6,7 @@ namespace Nexendrie\BookComponent;
 final class BookControl2 extends BookControl {
   public function __construct() {
     parent::__construct("Test", __DIR__);
-    $this->pages = function() {
+    $this->pages = function(): BookPagesStorage {
       $storage = new BookPagesStorage();
       $storage[] = new BookPage("slug1", "title1");
       $storage[] = new BookPage("slug2", "title2");
@@ -23,7 +23,7 @@ final class BookControl2 extends BookControl {
       $storage[] = $conditionalPage;
       return $storage;
     };
-    $this->onRender[] = function(BookControl $book, string $page) {
+    $this->onRender[] = function(BookControl $book, string $page): void {
       if($page === "slug1") {
         $book->template->var2 = "Dota.";
       }
